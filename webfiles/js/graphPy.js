9b7d78be3706a018c3fe80f752cdaf6446da793e
mazeitor <mazeitor@gmail.com>
2015-02-19 21:35:51 +0000

    added tools config management

--- webfiles/js/graphPy.js
+++ webfiles/js/graphPy.js
@@ -3,19 +3,23 @@ var data;
 var network;
 var timeline;
 
-//crear mapa de edges bidireccionals i no repetir !!!
-var edgesBidirectional = {};
-//var nodesMap = [];
 var nodesMap = new vis.DataSet();
 var edgesMap = new vis.DataSet();
-var items = new vis.DataSet();
 var groups = new vis.DataSet();
 var nodes, edges;
 
 var container = document.getElementById('network');
+var data = {};
+
+//layout config
+var enabledLayout = 0;
+var centralGravityValue = 1.5;
+var nodeDistanceValue = 100;
+
+//var ctrl = false;
 
 $(function () {
-	load(data);
+	load();
 });
 
 setInterval(
@@ -27,48 +31,164 @@ function paintUpdates(obj)
 {
 	for (index = 0; index < obj.length; index++)
 	{
-		if (obj[index][0]=='addNode')
-			{
+		switch(obj[index][0]) {
+			case 'addNode':
 				addNode(obj[index][1]);
-			}
-		else if (obj[index][0]=='addEdge')
-		{
-			addLink(obj[index][1],obj[index][2]);
+				break;
+			case 'addEdge':
+				addEdge(obj[index][1],obj[index][2]);
+				break;
+			case 'removeEdge':
+				removeNode(obj[index][1]);
+				break;
+			case 'removeEdge':
+				removeEdge(obj[index][1],obj[index][2]);
+				break;
 		}
 	}
 }
 
-function load(data) {
-
-	//load node
+function loadDataTest(){
+	//nodes
 	addNode("1");
 	addNode("2");
-	addLink("1","2");
+	addNode("3");
+	addNode("4");
 
-	// create a network
+	//edges
+	addEdge("1","1");
+	addEdge("1","2");
+	addEdge("1","3");
+	addEdge("1","4");
+	addEdge("2","3");
+	addEdge("2","4");
+	addEdge("3","4");
+}
+
+
+function load() {
+
+	//loas data
+	loadDataTest();
+
+	//create a network
 	container = document.getElementById('network');
 
-	var data = {
+	data = {
 		nodes: nodesMap,
 		edges: edgesMap
 	};
 
-	//var options = {stabilize: false};
-	var options = {configurePhysics:false, physics: {barnesHut: {springConstant: 0.018}}};
+	var options = {physics: {barnesHut: {enabled: true, centralGravity:centralGravityValue}}};
 	network = new vis.Network(container, data, options);
 
+	//add events listener
+	network.on('select', selectElement);
+	network.on('doubleClick', doubleClickElement);
+
+	//add event keyboard listener
+	//document.onkeydown = checkKey;
+	//document.onkeyup = checkKey;
 };
 
-function addNode(id){
-	group = 2;
-	title = "id: "+id;
-	var n = {"id":id,"label":id, "title":title};
+//NETWORK
+function addNode(id,label,title){
+	var n = {"id":id,"label":label, "title":title};
 	nodesMap.add(n);
 }
 
-function addLink(id1,id2){
-	var style = "line";
-	label = "label: "+id1+":"+id2;
-	var e = {"from":id1,"to":id2, "label": label, "style": style};
+function addEdge(id1,id2, label, title){
+	var e = {"id":id1+":"+id2,"from":id1,"to":id2, "label": label, "title":title};
 	edgesMap.add(e);
+}
+
+function removeNode(id){
+	var n = {"id":id};
+	nodesMap.remove(n);
+}
+
+function removeEdge(id1,id2){
+	var e = {"id":id1+":"+id2};
+	edgesMap.remove(e);
+}
+
+//EVENTS
+function selectElement(properties){
+	var idsNodes = properties.nodes;
+	var idsEdges = properties.edges;
+
+
+	//get keyboard pressed
+	//if(ctrl){
+		console.log("selectElement");
+		console.log(idsNodes);
+		console.log(idsEdges);
+	
+		changeLayout('2');
+
+	//}
+}
+
+function doubleClickElement(properties){
+	var idsNodes = properties.nodes;
+	var idsEdges = properties.edges;
+	console.log("doubleClickElement");
+	console.log(idsNodes);
+	console.log(idsEdges);
+}
+
+/*function checkKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '17') {
+        ctrl = !ctrl;
+    }
+}*/
+
+//FUNCTIONS
+function changeLayoutType(id){
+
+	switch(id) {
+		case "labelBarnesHutRadio":
+			enabledValue = 0;
+			break;
+		case "labelRepulsionRadio":
+			enabledValue = 1;
+			break;
+		case "labelHierarchicalRepulsionRadio":
+			enabledValue = 2;
+			break;
+	}
+	reDrawLayout();
+}
+
+function changeLayoutCentralGravity(value){
+	centralGravityValue  = value;
+	reDrawLayout();
+}
+
+function changeLayoutNodeDistance(value){
+	nodeDistanceValue = value;
+	reDrawLayout();
+}
+
+function reDrawLayout(){
+	console.log(enabledValue);
+	switch(enabledValue) {
+		case 0:
+			console.log(enabledValue);
+			var options = {physics: {barnesHut: {enabled: true, centralGravity:centralGravityValue}}};
+			network = new vis.Network(container, data, options);
+			break;
+		case 1:
+			console.log(enabledValue+":"+nodeDistanceValue);
+			var options = {physics: {barnesHut: {enabled: false}, repulsion: {nodeDistance: nodeDistanceValue, centralGravity: centralGravityValue}}};
+			network = new vis.Network(container, data, options);
+			break;
+		case 2:
+			console.log(enabledValue+":"+nodeDistanceValue);
+			var options = {physics: {hierarchicalRepulsion: {nodeDistance: nodeDistanceValue}}};
+			network = new vis.Network(container, data, options);
+			break;
+	}
 }
