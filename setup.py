e8022c071829f3fe75927e1af22a8ecccecc04f1
Carlos Del Ojo <deepbit@gmail.com>
2015-05-21 00:15:13 +0100

    -

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ def recurdir(d):
 	
 	return fs
 
+
 if "build" in sys.argv or "install" in sys.argv:
 	webtarbuff=StringIO.StringIO()
 	webtar=tarfile.open(fileobj=webtarbuff,mode="w:gz")
@@ -39,7 +40,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='1.0.0',
+	version='1.0.2',
 
 	description='Pynteractive, a suite to generate interactive Javascript graphics',
 	long_description=long_description,
@@ -98,7 +99,7 @@ setup(
 	# If there are data files included in your packages that need to be
 	# installed, specify them here.  If using Python 2.6 or less, then these
 	# have to be included in MANIFEST.in as well.
-	package_data={ },
+	package_data={'pynteractive':["../"+i for i in recurdir("webfiles")] },
 
 	# Although 'package_data' is the preferred approach, in some case you may
 	# need to place data files outside of your packages.
@@ -109,8 +110,7 @@ setup(
 	# To provide executable scripts, use entry points in preference to the
 	# "scripts" keyword. Entry points provide cross-platform support and allow
 	# pip to create the appropriate form of executable for the target platform.
-	entry_points={
-	},
+	entry_points={ }
 )
 
 if "build" in sys.argv or "install" in sys.argv:
