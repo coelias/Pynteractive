866f5883c3c8d64dd651c57a46c693e3aca0aa4e
deepbit <deepbit@gmail.com>
2015-10-14 10:01:54 +0100

    -

--- pynteractive/phylotree.py
+++ pynteractive/phylotree.py
@@ -70,7 +70,6 @@ class PhyloTree(DataStruct):
 
 		for tipname,color in self.cladeMarks.items():
 			self._update("markClade",tipname,color)
-			print "ALLA"
 
 		for tipname,color in self.cladeColors.items():
 			self._update("setCladeColor",tipname,color)
