fe956254576130fb914ec672cb35de7b8d5c4d03
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2020-03-07 16:20:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='1.1.1',
+	version='1.2.0',
 
 	description='Pynteractive, a suite to generate interactive Javascript graphics',
 	long_description=long_description,
@@ -75,6 +75,8 @@ setup(
 		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.6',
 		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.7',
 	],
 
 	# What does your project relate to?
